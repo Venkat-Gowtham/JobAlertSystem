a2873153caed5d566978916e1db186e5a36592eb
Kotamsettideepak <21A91A04D8@aec.edu.in>
2024-07-24 16:07:52 +0530

    test3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Test1 from "./Test1";
+import Test3 from "./Test3";
 function App() {
   return (
     <>
       <Test1 />
+      <Test3 />
       <div>hello world</div>
     </>
   );
---
+++ frontend/src/Test3.js
@@ -0,0 +1,6 @@
+import react from "react";
+function Test3() {
+  return <>Test 3</>;
+}
+
+export default Test3;
